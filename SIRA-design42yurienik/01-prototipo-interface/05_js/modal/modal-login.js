6eecaba8a0d5dc8314e7aba9aee2462f309f6b6a
Gustavo Tramontin Pedro <mrgustavotramontin@gmail.com>
2025-05-12 17:14:18 -0300

    fix structure

--- SIRA-design42yurienik/01-prototipo-interface/05_js/modal/modal-login.js
+++
@@ -1,6 +0,0 @@
-export function exibirLogin() {
-    document.getElementById("telaLogin").classList.remove("hidden");
-    document.getElementById("telaCadastro").classList.add("hidden");
-    document.getElementById("telaEsqueciSenha").classList.add("hidden");
-  }
-  
