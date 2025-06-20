d19613b9358cc4da859135498c5f5565e8888e66
yurienik <83026410+yurienik@users.noreply.github.com>
2025-06-20 02:30:11 -0300

    Ajuste estilo google agenda

--- new-front/app/components/reserva.jsx
+++ new-front/app/components/reserva.jsx
@@ -1,44 +1,45 @@
 import { Texto } from '../lib/texto.js'
 import { hora_para_texto } from '../lib/tempo.js'
-
-import { Button } from './form.jsx'
-
 import { useContext } from 'react'
 
 import styles from './visao-diaria.module.css'
 
 import { AbrirReservaModalContext } from './reservaContext.js'
 
-export default function Reserva({matéria, dia, início, duração, lab, vazia}) {
+export default function Reserva({ matéria, dia, início, duração, lab, vazia }) {
+  const abrirReserva = useContext(AbrirReservaModalContext)
 
-    const abrirReserva = useContext(AbrirReservaModalContext)
-
-    const topo = Math.max((início - 8)*60 + 1, 2)
-
-    const corpo = Texto.limitado(matéria, 40)
+  const topo = Math.max((início - 8) * 60 + 1, 2)
+  const corpo = Texto.limitado(matéria, 40)
 
   const posicionamento = {
     gridRow: topo,
     gridRowEnd: 'span ' + duração,
-    gridColumn: lab+1
+    gridColumn: lab + 1,
   }
 
   const labs_names = ["A03", "A04", "D04", "D05", "D06", "D07"]
   const opts = {
     dia: dia,
     início: hora_para_texto(início),
-    fim: hora_para_texto(início+(duração/60)),
-    lab: labs_names[lab-1]
+    fim: hora_para_texto(início + duração / 60),
+    lab: labs_names[lab - 1],
   }
 
-    if (vazia)
-      return  <div className={styles.reserva_vazia} style={posicionamento}>
-      <Button desc="+" onClick={() => abrirReserva(opts)}/>
-    </div>
+  if (vazia) {
     return (
+      <div
+        className={`${styles.reserva_vazia} ${styles.vazio}`}
+        style={posicionamento}
+        onClick={() => abrirReserva(opts)}
+        title={`Clique para reservar ${opts.lab} às ${opts.início}`}
+      />
+    )
+  }
 
-      <div title={matéria} className={styles.reserva} style={posicionamento}>
-        {corpo}
-      </div>
+  return (
+    <div title={matéria} className={styles.reserva} style={posicionamento}>
+      {corpo}
+    </div>
   )
 }
