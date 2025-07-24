92c596712aef48ff71b1f2d706f2bb52636b64f3
Hylson <hvescovi@gmail.com>
2025-07-24 15:34:46 -0300

    reservation pytest

---
+++ 02-bff/test_reservation.py
@@ -0,0 +1,29 @@
+import pytest
+
+from config import *
+from service.common_service import *
+from service.reservation_service import *
+
+def test_creation():
+    with app.app_context():
+
+        # Create a new object
+        room = create_object(Room, name="Lab A04")
+        user = create_object(User, name="John Silva", 
+                            email="jo@gmail.com", password="123")
+        obj = create_reservation(room, user, purpose="Matemática 201 info")   
+
+        assert obj.id is not None
+        assert isinstance(obj.id, UUID)
+        assert obj.purpose == "Matemática 201 info"
+        assert obj.active is True
+
+#def test_obj_delete():
+#    with app.app_context():
+ 
+        # testing delete method
+        uuid = obj.id
+        obj = get_object_by_id(Reservation, uuid)
+        assert obj.purpose == "Matemática 201 info"
+        ok = delete_object(obj)
+        assert ok == True
