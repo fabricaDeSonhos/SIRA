dab3a99445a26550fdee6675c7c86e2548a331b8
manuzickuhr <manuelalopeszickuhr@gmail.com>
2025-08-14 18:38:48 -0300

    update new - menu

---
+++ new-front/app/components/header.jsx
@@ -0,0 +1,36 @@
+"use client"; // necessário para hooks do Next.js, se precisar do pathname
+
+import Link from "next/link";
+import styles from "./header.module.css";
+
+export default function Header() {
+  return (
+    <header className={styles.headerContainer}>
+      <h1 className={styles.logo}>SIRA</h1>
+      <nav className={styles.navbar}>
+        <ul>
+          <li><Link href="/">Diária</Link></li>
+          <li>
+            <Link href="/semanal">Semanal</Link>
+          </li>
+          <li>
+            <Link href="/listagem">Listagem</Link>
+          </li>
+          <li>
+            <Link href="/reservar">Reservar</Link>
+          </li>
+          <li>
+            <Link href="/admin" className={styles.highlight}>
+              Admin
+            </Link>
+          </li>
+          <li>
+            <Link href="/primeiro-acesso" className={styles.highlight}>
+              Primeiro Acesso
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
--- new-front/app/layout.js
+++ new-front/app/layout.js
@@ -1,5 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./css/globals.css";
+import Header from "./components/header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -13,13 +14,15 @@ const geistMono = Geist_Mono({
 
 export const metadata = {
   title: "SIRA - Sistema de Reserva de Ambiente",
-  description: "SIRA - Sistemas de Reserva de Ambientes um projeto de extensão para o IFC campus Blumenau, um sistema visando resolver o problema de alocações de salas de informática do campus ",
+  description:
+    "SIRA - Sistemas de Reserva de Ambientes um projeto de extensão para o IFC campus Blumenau, um sistema visando resolver o problema de alocações de salas de informática do campus ",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="pt-br">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
+        <Header />
         {children}
       </body>
     </html>
