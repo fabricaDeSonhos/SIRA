392021c8d6841816643d8883596d33a63aec1dd1
Gustavo Tramontin Pedro <mrgustavotramontin@gmail.com>
2025-08-17 01:09:48 -0300

    sql script to insert labs

---
+++ 02-bff/back/setup/default_rooms.sql
@@ -0,0 +1,7 @@
+
+insert into rooms values ('1', 'A03', '1');
+insert into rooms values ('2', 'A04', '1');
+insert into rooms values ('3', 'D04', '1');
+insert into rooms values ('4', 'D05', '1');
+insert into rooms values ('5', 'D06', '1');
+insert into rooms values ('6', 'D07', '1');
