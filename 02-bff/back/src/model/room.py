97f15d95321541b9692db59a99a9bb908bed2faa
Hylson <hvescovi@gmail.com>
2025-09-01 16:58:29 -0300

    added field "type" in Room

--- 02-bff/back/src/model/room.py
+++ 02-bff/back/src/model/room.py
@@ -8,6 +8,9 @@ class Room(db.Model):
     name: Mapped[str] = mapped_column(String(1024), nullable=False)
     active: Mapped[bool] = mapped_column(db.Boolean, default=True)
     
+    # Sala de Aula, Laboratório de Informática, Ginásio, Auditório, Laboratório de Química, 
+    type: Mapped[str] = mapped_column(String(150), nullable=False, default="Sala de Aula")
+    
     reservations: Mapped[List["Reservation"]] = db.relationship("Reservation", back_populates="room")
     
     def __repr__(self):
